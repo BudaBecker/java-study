d0ece7fa92151ac46c6453250997bc4e5fd48fa3
BudaBecker <cacocidral@hotmail.com>
2025-06-02 19:44:04 -0300

    Refactor to use helper methods for instantiating Department and Seller objects

--- demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -52,17 +52,9 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
 
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
 
-                Seller sel = new Seller();
-                sel.setId(rs.getInt("Id"));
-                sel.setName(rs.getString("Name"));
-                sel.setEmail(rs.getString("Email"));
-                sel.setBaseSalary(rs.getDouble("BaseSalary"));
-                sel.setBirthDate(rs.getDate("BirthDate"));
-                sel.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller sel = instantiateSeller(rs, dep);
 
                 return sel;
             }
@@ -83,4 +75,22 @@ public class SellerDaoJDBC implements SellerDao {
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
 
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller sel = new Seller();
+        sel.setId(rs.getInt("Id"));
+        sel.setName(rs.getString("Name"));
+        sel.setEmail(rs.getString("Email"));
+        sel.setBaseSalary(rs.getDouble("BaseSalary"));
+        sel.setBirthDate(rs.getDate("BirthDate"));
+        sel.setDepartment(dep);
+
+        return sel;
+    }
 }
