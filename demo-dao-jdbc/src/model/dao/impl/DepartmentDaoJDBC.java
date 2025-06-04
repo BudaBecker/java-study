087a26414d0668046b4a551f5dadf3d207bf948b
BudaBecker <cacocidral@hotmail.com>
2025-06-03 22:16:37 -0300

    Implement update method

--- demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -50,8 +50,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department dp) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE department "
+                            + "SET Name = ? "
+                            + "WHERE Id = ?");
+            st.setString(1, dp.getName());
+            st.setInt(2, dp.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
