6b144106901d060dbb2adc1840de4ceb48167ba8
BudaBecker <cacocidral@hotmail.com>
2025-06-08 15:38:19 -0300

    Implement UserService for user management and update UserResource to utilize the service

---
+++ Spring-study/src/main/java/com/udemy/Spring_study/services/UserService.java
@@ -0,0 +1,26 @@
+package com.udemy.Spring_study.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.Spring_study.entities.User;
+import com.udemy.Spring_study.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
