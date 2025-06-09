7332e3e198b72898cd8dfb8d62c5f1b4791429b7
BudaBecker <cacocidral@hotmail.com>
2025-06-08 21:20:39 -0300

    Add Payment entity and establish 1-1 relationship with Order

--- Spring-study/src/main/java/com/udemy/Spring_study/config/TestConfig.java
+++ Spring-study/src/main/java/com/udemy/Spring_study/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.Spring_study.entities.Category;
 import com.udemy.Spring_study.entities.Order;
 import com.udemy.Spring_study.entities.OrderItem;
+import com.udemy.Spring_study.entities.Payment;
 import com.udemy.Spring_study.entities.Product;
 import com.udemy.Spring_study.entities.User;
 import com.udemy.Spring_study.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
